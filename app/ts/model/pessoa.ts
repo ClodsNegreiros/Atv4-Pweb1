c2809295b20707b8326b2777667a85399dcbb35b
ClodsNegreiros <clodsn@gmail.com>
2023-09-17 22:36:58 -0300

    feat: Criando classe pessoa e suas sub-classes

---
+++ app/ts/model/pessoa.ts
@@ -0,0 +1,36 @@
+class Pessoa {
+
+    private nome: string;
+    private idade: number;
+    private dataNascimento: Date;
+
+    constructor(nome: string, idade: number, dataNascimento: Date) {
+        this.nome = nome;
+        this.idade = idade;
+        this.dataNascimento = dataNascimento;
+    }
+
+    get _nome(): string {
+        return this.nome
+    }
+
+    set _nome(novoNome: string) {
+        this.nome = novoNome;
+    }
+
+    get _idade(): number {
+        return this.idade
+    }
+
+    set _idade(novaIdade: number) {
+        this.idade = novaIdade;
+    }
+
+    get _dataNascimento(): Date {
+        return this.dataNascimento
+    }
+
+    set _dataNascimento(novaDataNascimento: Date) {
+        this.dataNascimento = novaDataNascimento;
+    }
+}
